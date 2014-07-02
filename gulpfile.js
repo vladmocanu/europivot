221dce2e790482557e4d8c43d81ecaa7bc39ba19
Vlad Mocanu <vlad.mocanu@me.com>
2014-07-02 11:59:16 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var gulp = require('gulp');
 
 // CSS minify and move
 gulp.task('move', function() {
-  gulp.src(['./_site/css/main.css'])
+  gulp.src(['./_site/css/*.css'])
     .pipe(gulp.dest('./css/'));
 });
 
 // Watch for changes
 gulp.task('watch', function() {
-	gulp.watch('./_site/css/main.css', ['move']);
+	gulp.watch('./_site/css/*.css', ['move']);
 });
 
 // Default Task
