77cda4ff070e7c553b0d326319c39b407648c8e4
Vlad Mocanu <vlad.mocanu@me.com>
2014-07-02 11:54:57 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,11 @@ gulp.task('move', function() {
   gulp.src(['./_site/css/main.css'])
     .pipe(gulp.dest('./css/'));
 });
+
+// Watch for changes
+gulp.task('watch', function() {
+	gulp.watch('./_site/css/main.css', ['move']);
+});
+
+// Default Task
+gulp.task('default', ['move', 'watch']);
